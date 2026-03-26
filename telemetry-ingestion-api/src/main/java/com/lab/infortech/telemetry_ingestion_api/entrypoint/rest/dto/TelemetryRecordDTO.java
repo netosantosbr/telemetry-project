ae35683b6427d90e4b77eac5730062c960106861
José Alfredo <netoalfredo9@gmail.com>
2026-03-26 00:26:48 -0300

    feat(ingestion): DTO created using record and validation constraints

--- telemetry-ingestion-api/src/main/java/com/lab/infortech/telemetry_ingestion_api/entrypoint/rest/dto/TelemetryRecordDTO.java
+++ telemetry-ingestion-api/src/main/java/com/lab/infortech/telemetry_ingestion_api/entrypoint/rest/dto/TelemetryRecordDTO.java
@@ -1,4 +1,18 @@
 package com.lab.infortech.telemetry_ingestion_api.entrypoint.rest.dto;
 
-public class TelemetryRecordDTO {
-}
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import java.time.Instant;
+
+public record TelemetryRecordDTO(
+    @NotNull
+    Integer patientId,
+    @NotNull @Positive
+    Integer heartRate,
+    @NotNull @Positive
+    Integer systolicBloodPressure,
+    @NotNull @Positive
+    Integer diastolicBloodPressure,
+    @NotNull
+    Instant captureTime
+) {}
