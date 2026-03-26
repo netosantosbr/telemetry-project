a5f9ef75370df476e4fc8cb95d34fd1c5adcdd04
José Alfredo <netoalfredo9@gmail.com>
2026-03-26 00:42:49 -0300

    feat(usecase): implementing IngestTelemetryUseCase

--- telemetry-ingestion-api/src/main/java/com/lab/infortech/telemetry_ingestion_api/application/IngestTelemetryUseCase.java
+++ telemetry-ingestion-api/src/main/java/com/lab/infortech/telemetry_ingestion_api/application/IngestTelemetryUseCase.java
@@ -1,4 +1,24 @@
 package com.lab.infortech.telemetry_ingestion_api.application;
 
+import com.lab.infortech.telemetry_ingestion_api.domain.Telemetry;
+import com.lab.infortech.telemetry_ingestion_api.entrypoint.rest.dto.TelemetryRecordDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
 public class IngestTelemetryUseCase {
+    public void execute(TelemetryRecordDTO telemetryRecordDTO) {
+        Telemetry telemetry = Telemetry.builder()
+                .patientId(telemetryRecordDTO.patientId())
+                .heartRate(telemetryRecordDTO.heartRate())
+                .systolicBloodPressure(telemetryRecordDTO.systolicBloodPressure())
+                .diastolicBloodPressure(telemetryRecordDTO.diastolicBloodPressure())
+                .captureTime(telemetryRecordDTO.captureTime())
+                .build();
+
+        if(telemetry.isCritical()) {
+            log.info("ALERTA: Paciente ID {} está em estado crítico!!", telemetry.getPatientId());
+        }
+    }
 }
