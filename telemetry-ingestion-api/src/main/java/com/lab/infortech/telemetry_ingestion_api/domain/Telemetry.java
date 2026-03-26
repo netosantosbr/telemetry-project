ae730eddfa7e553d0ec2f5535c84ba1151059210
José Alfredo <netoalfredo9@gmail.com>
2026-03-25 23:53:56 -0300

    feat(domain): created telemetry model

--- telemetry-ingestion-api/src/main/java/com/lab/infortech/telemetry_ingestion_api/domain/Telemetry.java
+++ telemetry-ingestion-api/src/main/java/com/lab/infortech/telemetry_ingestion_api/domain/Telemetry.java
@@ -1,4 +1,20 @@
 package com.lab.infortech.telemetry_ingestion_api.domain;
 
+import lombok.*;
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Telemetry {
+    private Integer patientId;
+    private Integer heartRate;
+    private Integer systolicBloodPressure;
+    private Integer diastolicBloodPressure;
+    private Instant captureTime;
+
+    public Boolean isCritical() {
+        if(heartRate == null) return false;
+        return heartRate < 50 || heartRate > 120;
+    }
 }
