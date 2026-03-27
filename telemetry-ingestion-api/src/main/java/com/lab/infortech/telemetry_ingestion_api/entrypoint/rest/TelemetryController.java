245cd3685ccab38cc10966be8e20504f33c5d606
José Alfredo <netoalfredo9@gmail.com>
2026-03-26 22:00:14 -0300

    feat(rest): implementing controller class

--- telemetry-ingestion-api/src/main/java/com/lab/infortech/telemetry_ingestion_api/entrypoint/rest/TelemetryController.java
+++ telemetry-ingestion-api/src/main/java/com/lab/infortech/telemetry_ingestion_api/entrypoint/rest/TelemetryController.java
@@ -1,4 +1,27 @@
 package com.lab.infortech.telemetry_ingestion_api.entrypoint.rest;
 
+import com.lab.infortech.telemetry_ingestion_api.application.IngestTelemetryUseCase;
+import com.lab.infortech.telemetry_ingestion_api.entrypoint.rest.dto.TelemetryRecordDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/telemetry")
 public class TelemetryController {
+
+    private final IngestTelemetryUseCase ingestTelemetryUseCase;
+
+    public TelemetryController(IngestTelemetryUseCase ingestTelemetryUseCase) {
+        this.ingestTelemetryUseCase = ingestTelemetryUseCase;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> sendTelemetry(@RequestBody @Valid TelemetryRecordDTO telemetryRecordDTO) {
+        this.ingestTelemetryUseCase.execute(telemetryRecordDTO);
+        return ResponseEntity.accepted().build();
+    }
 }
