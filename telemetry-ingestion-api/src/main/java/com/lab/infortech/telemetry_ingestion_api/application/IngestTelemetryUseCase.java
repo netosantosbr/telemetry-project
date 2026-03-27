d8dd3dbfdacfb71467e5402ff7258552be292d07
José Alfredo <netoalfredo9@gmail.com>
2026-03-26 21:42:55 -0300

    feat(usecase): calling produceMessage in UseCase

--- telemetry-ingestion-api/src/main/java/com/lab/infortech/telemetry_ingestion_api/application/IngestTelemetryUseCase.java
+++ telemetry-ingestion-api/src/main/java/com/lab/infortech/telemetry_ingestion_api/application/IngestTelemetryUseCase.java
@@ -2,12 +2,20 @@ package com.lab.infortech.telemetry_ingestion_api.application;
 
 import com.lab.infortech.telemetry_ingestion_api.domain.Telemetry;
 import com.lab.infortech.telemetry_ingestion_api.entrypoint.rest.dto.TelemetryRecordDTO;
+import com.lab.infortech.telemetry_ingestion_api.infrastructure.messaging.KafkaTelemetryProducer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class IngestTelemetryUseCase {
+
+    private final KafkaTelemetryProducer kafkaTelemetryProducer;
+
+    public IngestTelemetryUseCase(KafkaTelemetryProducer kafkaTelemetryProducer) {
+        this.kafkaTelemetryProducer = kafkaTelemetryProducer;
+    }
+
     public void execute(TelemetryRecordDTO telemetryRecordDTO) {
         Telemetry telemetry = Telemetry.builder()
                 .patientId(telemetryRecordDTO.patientId())
@@ -20,5 +28,7 @@ public class IngestTelemetryUseCase {
         if(telemetry.isCritical()) {
             log.info("ALERTA: Paciente ID {} está em estado crítico!!", telemetry.getPatientId());
         }
+
+        kafkaTelemetryProducer.produceMessage(telemetry);
     }
 }
