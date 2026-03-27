107c0a30052b87daac9d1531d1613e3c5e48a4d5
José Alfredo <netoalfredo9@gmail.com>
2026-03-26 21:37:46 -0300

    feat(kafka): implementing kafka producer

--- telemetry-ingestion-api/src/main/java/com/lab/infortech/telemetry_ingestion_api/infrastructure/messaging/KafkaTelemetryProducer.java
+++ telemetry-ingestion-api/src/main/java/com/lab/infortech/telemetry_ingestion_api/infrastructure/messaging/KafkaTelemetryProducer.java
@@ -1,4 +1,22 @@
 package com.lab.infortech.telemetry_ingestion_api.infrastructure.messaging;
 
+import com.lab.infortech.telemetry_ingestion_api.domain.Telemetry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
 public class KafkaTelemetryProducer {
+    private final KafkaTemplate<String, Telemetry> kafkaTemplate;
+    private static final String KAFKA_TOPIC = "telemetry-ingestion-topic";
+
+    public KafkaTelemetryProducer(KafkaTemplate<String, Telemetry> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void produceMessage(Telemetry telemetry) {
+        kafkaTemplate.send(KAFKA_TOPIC, telemetry.getPatientId().toString(), telemetry);
+        log.info("Enviando telemetria do paciente {} para o Kafka", telemetry.getPatientId());
+    }
 }
