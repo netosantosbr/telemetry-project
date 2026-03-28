9cb493c288e200e5e908f4e5b36d623ebedfbc08
José Alfredo <netoalfredo9@gmail.com>
2026-03-28 01:54:41 -0300

    feat: inserting telemetry monitoring service infrastructure

---
+++ telemetry-monitoring-api/src/main/java/com/lab/infortech/telemetry_monitoring_api/TelemetryMonitoringApiApplication.java
@@ -0,0 +1,13 @@
+package com.lab.infortech.telemetry_monitoring_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemetryMonitoringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemetryMonitoringApiApplication.class, args);
+	}
+
+}
---
+++ telemetry-monitoring-api/src/test/java/com/lab/infortech/telemetry_monitoring_api/TelemetryMonitoringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.infortech.telemetry_monitoring_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemetryMonitoringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
