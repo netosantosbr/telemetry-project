5f3f5af8f9f4d44dd06aee498f0fe02824352827
José Alfredo <netoalfredo9@gmail.com>
2026-03-29 00:05:39 -0300

    refac(domain): modifying telemetry entity

--- telemetry-monitoring-api/src/main/java/com/lab/infortech/telemetry_monitoring_api/domain/TelemetryEntity.java
+++ telemetry-monitoring-api/src/main/java/com/lab/infortech/telemetry_monitoring_api/domain/TelemetryEntity.java
@@ -1,13 +1,19 @@
 package com.lab.infortech.telemetry_monitoring_api.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.TimeSeries;
 
 import java.time.Instant;
 
-@TimeSeries(collection = "telemetry", timeField = "captureTime", metaField = "patientId")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@TimeSeries(collection = "telemetry", timeField = "captureTime", metaField = "patientId")
 public class TelemetryEntity {
     @Id
     private String id;
