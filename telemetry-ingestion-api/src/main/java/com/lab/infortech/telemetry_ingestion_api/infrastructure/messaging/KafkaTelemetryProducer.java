a4651228995a5bc660859dd784e25324aae6b4bf
José Alfredo <netoalfredo9@gmail.com>
2026-04-19 15:40:08 -0300

    feat: adding keycloak authentication in security layer

--- telemetry-ingestion-api/src/main/java/com/lab/infortech/telemetry_ingestion_api/infrastructure/messaging/KafkaTelemetryProducer.java
+++ telemetry-ingestion-api/src/main/java/com/lab/infortech/telemetry_ingestion_api/infrastructure/messaging/KafkaTelemetryProducer.java
@@ -3,6 +3,11 @@ package com.lab.infortech.telemetry_ingestion_api.infrastructure.messaging;
 import com.lab.infortech.telemetry_ingestion_api.domain.Telemetry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -16,7 +21,19 @@ public class KafkaTelemetryProducer {
     }
 
     public void produceMessage(Telemetry telemetry) {
-        kafkaTemplate.send(KAFKA_TOPIC, telemetry.getPatientId().toString(), telemetry);
+        var authentication = (JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+
+        String tokenJwt = authentication.getToken().getTokenValue();
+
+        Message<Telemetry> kafkaMessage = MessageBuilder
+                .withPayload(telemetry)
+                .setHeader(KafkaHeaders.TOPIC, KAFKA_TOPIC)
+                .setHeader(KafkaHeaders.KEY, telemetry.getPatientId().toString())
+                .setHeader("Authorization", "Bearer " + tokenJwt)
+                .build();
+
+        kafkaTemplate.send(kafkaMessage);
+
         log.info("Enviando telemetria do paciente {} para o Kafka", telemetry.getPatientId());
     }
 }
