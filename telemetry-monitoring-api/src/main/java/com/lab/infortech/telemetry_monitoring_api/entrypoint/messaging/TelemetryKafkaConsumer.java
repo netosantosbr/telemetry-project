f5d406aec72a04dc4fbc964bdb4f5efe10bb70e4
José Alfredo <netoalfredo9@gmail.com>
2026-04-19 20:13:15 -0300

    feat: implementing an extra layer of protection for the consumer

--- telemetry-monitoring-api/src/main/java/com/lab/infortech/telemetry_monitoring_api/entrypoint/messaging/TelemetryKafkaConsumer.java
+++ telemetry-monitoring-api/src/main/java/com/lab/infortech/telemetry_monitoring_api/entrypoint/messaging/TelemetryKafkaConsumer.java
@@ -6,7 +6,12 @@ import com.lab.infortech.telemetry_monitoring_api.infrastructure.repository.Tele
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -14,25 +19,53 @@ import org.springframework.stereotype.Component;
 public class TelemetryKafkaConsumer {
 
     private final TelemetryRepository telemetryRepository;
+    private final JwtDecoder jwtDecoder;
 
-    public TelemetryKafkaConsumer(TelemetryRepository telemetryRepository) {
+    public TelemetryKafkaConsumer(TelemetryRepository telemetryRepository, JwtDecoder jwtDecoder) {
         this.telemetryRepository = telemetryRepository;
+        this.jwtDecoder = jwtDecoder;
     }
 
     @KafkaListener(groupId = "${spring.kafka.consumer.group-id}", topics = "${spring.kafka.topic.telemetry}")
-    public void consume(@Payload @Valid TelemetryMessage telemetryMessage) {
+    public void consume(@Payload @Valid TelemetryMessage telemetryMessage,
+                        @Header(value = "Authorization", required = false) String authHeader) {
         log.info("Recebida telemetria do paciente ID: {} | Batimentos: {} bpm",
                 telemetryMessage.patientId(),
                 telemetryMessage.heartRate());
 
-        TelemetryEntity telemetryEntity = TelemetryEntity.builder()
-                .patientId(telemetryMessage.patientId())
-                .heartRate(telemetryMessage.heartRate())
-                .diastolicBloodPressure(telemetryMessage.diastolicBloodPressure())
-                .systolicBloodPressure(telemetryMessage.systolicBloodPressure())
-                .captureTime(telemetryMessage.captureTime())
-                .build();
+        if(authHeader == null || !authHeader.startsWith("Bearer")) {
+            log.error("Acesso negado: Tentativa de injeção de dados sem JWT.");
+            throw new SecurityException("Mensagem rejeitada: Cabeçalho Authorization ausente ou inválido.");
+        }
 
-        this.telemetryRepository.save(telemetryEntity);
+        String tokenRaw = authHeader.substring(7);
+
+        try {
+            Jwt jwt = jwtDecoder.decode(tokenRaw);
+
+            JwtAuthenticationToken authentication = new JwtAuthenticationToken(jwt);
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            log.info("Token validado com sucesso! Processando telemetria do paciente: {}", telemetryMessage.patientId());
+
+            TelemetryEntity telemetryEntity = TelemetryEntity.builder()
+                    .patientId(telemetryMessage.patientId())
+                    .heartRate(telemetryMessage.heartRate())
+                    .diastolicBloodPressure(telemetryMessage.diastolicBloodPressure())
+                    .systolicBloodPressure(telemetryMessage.systolicBloodPressure())
+                    .captureTime(telemetryMessage.captureTime())
+                    .build();
+
+            this.telemetryRepository.save(telemetryEntity);
+
+        } catch(Exception e) {
+            log.error("Falha de segurança na mensagem do Kafka: {}", e.getMessage());
+            throw new SecurityException("Token JWT inválido, adulterado ou expirado.", e);
+        } finally {
+            SecurityContextHolder.clearContext();
+        }
+
+
     }
 }
