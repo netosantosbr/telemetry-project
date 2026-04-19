ffec7e3a4908d5d19a744292275b49387689c0d3
José Alfredo <netoalfredo9@gmail.com>
2026-04-19 14:29:40 -0300

    feat: adding thread context propagation

---
+++ telemetry-monitoring-api/src/main/java/com/lab/infortech/telemetry_monitoring_api/infrastructure/config/AsyncConfig.java
@@ -0,0 +1,28 @@
+package com.lab.infortech.telemetry_monitoring_api.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.support.ContextPropagatingTaskDecorator;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(50);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("AsyncTelemetria-");
+
+        executor.setTaskDecorator(new ContextPropagatingTaskDecorator());
+
+        executor.initialize();
+
+        return executor;
+    }
+}
